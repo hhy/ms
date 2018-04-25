483f277393f5cf27cb16a2695136b681c9fc8a61
haiyang huang <haiyang.huang.ca@gmail.com>
2018-04-25 06:54:12 -0400

    a

--- src/main/java/pa/Rest.java
+++ src/main/java/pa/Rest.java
@@ -1,13 +1,26 @@
 package pa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController("/rest")
+
+
+@RestController
+@RequestMapping("/rest")
 public class Rest {
-	//@RequestMapping("/index")
-    public String index() {
-        return "Greetings from Spring Boot!";
+	
+	@RequestMapping("/aa/{id}")
+	//@Produces(XML)
+    public List<String> aa(@PathVariable String id) {
+		List<String> ss=new ArrayList<>();
+		ss.add("Greetings from Spring Boot!");
+		ss.add("Spring Boot!"+id);
+		return ss;
+        
     }
 
 }
